5be8fbac2b5a7cc229ef9eaa671b3ee5aa7e378c
Isaac Lopez <isaac.lopez.1b@gmail.com>
2024-10-22 17:29:42 -0600

    Initial Commit

---
+++ MoviesStoriChallenge/MoviesStoriChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesStoriChallengeApp.swift
+//  MoviesStoriChallenge
+//
+//  Created by Gustavo Isaac Lopez Nunez on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesStoriChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
